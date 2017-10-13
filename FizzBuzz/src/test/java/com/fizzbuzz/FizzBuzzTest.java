d2c3c43fdcb4aabf9e40f48c265946aeb193463c
Anshuli <anshuli.singhal@tcs.com>
2017-10-13 14:56:32 +0530

    Initial Project Commit

---
+++ FizzBuzz/src/test/java/com/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,23 @@
+package com.fizzbuzz;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class FizzBuzzTest {
+
+	public FizzBuzzTest() {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		
+		
+	}
+
+}
